cdcd33187c5ae69325dac3319505963fe8c17396
Omar Houmz <omhoumz@gmail.com>
2020-06-21 20:04:35 +0100

    feat: build site on heroes upadted

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
+const fetch = require('node-fetch')
+
 if (process.env.NODE_ENV === 'production') {
   admin.initializeApp()
 } else {
@@ -11,6 +13,10 @@ if (process.env.NODE_ENV === 'production') {
     databaseURL: 'https://popular-superheroes.firebaseio.com',
   })
 }
+
+const NETLIFY_BUILD_HOOK = functions.config().netlify
+  ? functions.config().netlify.build_hook
+  : ''
 
 exports.getDcHeroes = functions.https.onRequest((request, response) => {
   if (process.env.NODE_ENV !== 'production') {
@@ -36,3 +42,32 @@ exports.getDcHeroes = functions.https.onRequest((request, response) => {
       response.status(500).json({ message: 'Wuuut happened' })
     })
 })
+
+function triggerBuild() {
+  const requestOptions = {
+    method: 'POST',
+  }
+
+  fetch(NETLIFY_BUILD_HOOK, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error))
+}
+
+exports.triggerBuildOnDcHeroesCreated = functions.firestore
+  .document('dc-heroes/{heroId}')
+  .onCreate(async (snapshot) => {
+    triggerBuild()
+  })
+
+exports.triggerBuildOnDcHeroesUpdated = functions.firestore
+  .document('dc-heroes/{heroId}')
+  .onUpdate(async (change) => {
+    // const newValue = change.after.data()
+    // const name = newValue.name
+
+    // ...or the previous value before this update
+    // const previousValue = change.before.data()
+
+    triggerBuild()
+  })
